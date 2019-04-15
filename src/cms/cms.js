57ba0f24382f3788ca6fc3059b58438f43eb7877
Reginald Galang <reguv760@gmail.com>
2019-04-15 10:21:06 -0700

    update readme

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,6 +1,4 @@
 import CMS from 'netlify-cms'
-//import { Banner } from './../components/banner'
 import BannerPreview from './preview-template/BannerPreview'
 
 CMS.registerPreviewTemplate('banner', BannerPreview)
-//CMS.registerWidget('bannerComponent', Banner, BannerPreview)
