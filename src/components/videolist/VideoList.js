f48165dd7c00dd289e759185af6ec736f306c83f
Reginald Galang <reguv760@gmail.com>
2019-04-15 13:47:54 -0700

    display CMS to VideoList component in /videos/

--- src/components/videolist/VideoList.js
+++ src/components/videolist/VideoList.js
@@ -1,22 +1,19 @@
 import React from 'react'
-import { HTMLContent } from '../Content'
 
 export const VideoListTemplate = ({ videoListData }) => {
-  //const bannerTitle = bannerData.bannerTitle
-  //const bannerText = bannerData.bannerText
-
-  const PageContent = HTMLContent
+  //this is my video array data location
+  const videoItems = [videoListData.videos][0]
 
   return (
     <div className="inner">
-      <header className="major">
-        <h1>
-          {/* <PageContent className="bannerTitle" content={bannerTitle} /> */}
-        </h1>
-      </header>
-      <div className="content">
-        {/* <PageContent className="bannerText" content={bannerText} /> */}
-      </div>
+      <ul style={{ listStyle: 'none' }}>
+        {videoItems.map((videos, index) => (
+          <li key={videos.videoTitle + index}>
+            <h4>{videos.videoTitle}</h4>
+            <p>{videos.videoLink}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
