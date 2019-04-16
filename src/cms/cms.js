bebf60b1a7a1d9a028ce92edd4839c447d6f5974
Reginald Galang <reguv760@gmail.com>
2019-04-16 09:54:20 -0700

    register style to CMS

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,6 +3,6 @@ import BannerPreview from './preview-templates/BannerPreview'
 import VideoListPreview from './preview-templates/VideoListPreview'
 import siteStyle from './../assets/scss/main.scss'
 
-CMS.registerPreviewStyle(siteStyle)
 CMS.registerPreviewTemplate('banner', BannerPreview)
 CMS.registerPreviewTemplate('videoList', VideoListPreview)
+CMS.registerPreviewStyle(siteStyle)
