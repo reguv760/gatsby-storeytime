86d19f7a07e4f21abf3b4330d3674d602b1e0169
Reginald Galang <reguv760@gmail.com>
2019-04-16 09:26:40 -0700

    fix cms + video preview

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,6 +1,6 @@
 import CMS from 'netlify-cms'
-import BannerPreview from './preview-template/BannerPreview'
-import VideoListPreview from './preview-template/VideoListPreview'
+import BannerPreview from './preview-templates/BannerPreview'
+import VideoListPreview from './preview-templates/VideoListPreview'
 
 CMS.registerPreviewTemplate('banner', BannerPreview)
 CMS.registerPreviewTemplate('videoList', VideoListPreview)
