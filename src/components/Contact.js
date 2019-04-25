a521281687e7a7d4a2ab4579a38572e8ede0548e
Reginald Galang <reguv760@gmail.com>
2019-04-25 12:32:56 -0700

    integrate contactInfo to CMS

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,71 +1,77 @@
 import React from 'react'
+import { HTMLContent } from './Content'
 
-const Contact = props => (
-  console.log(props);
-  <section id="contact">
-    <div className="inner">
+export const ContactInfo = ({ contactInfoData }) => {
+  const { contactEmail, contactPhone, contactAddress } = contactInfoData
+  const PageContent = HTMLContent
+  return (
+    <section className="split">
       <section>
-        <form
-          method="POST"
-          data-netlify="true"
-          action="/success"
-          name="contact"
-        >
-          <input type="hidden" name="form-name" value="contact" />
-          <div className="field half first">
-            <label htmlFor="name">Name</label>
-            <input type="text" name="name" id="name" />
-          </div>
-          <div className="field half">
-            <label htmlFor="email">Email</label>
-            <input type="text" name="email" id="email" />
-          </div>
-          <div className="field">
-            <label htmlFor="message">Message</label>
-            <textarea name="message" id="message" rows="6" />
-          </div>
+        <div className="contact-method">
+          <span className="icon alt fa-envelope" />
+          <h3>Email</h3>
+          <a href={`mailto:${contactEmail}`}>{`${contactEmail}`}</a>
+        </div>
+      </section>
+      <section>
+        <div className="contact-method">
+          <span className="icon alt fa-phone" />
+          <h3>Phone</h3>
+          <span>{`${contactPhone}`}</span>
+        </div>
+      </section>
+      <section>
+        <div className="contact-method">
+          <span className="icon alt fa-home" />
+          <h3>Address</h3>
+          <span>
+            <PageContent content={`${contactAddress}`} />
+          </span>
+        </div>
+      </section>
+    </section>
+  )
+}
 
-          <ul className="actions">
-            <li>
-              <input type="submit" value="Send Message" className="special" />
-            </li>
-            <li>
-              <input type="reset" value="Clear" />
-            </li>
-          </ul>
-        </form>
-      </section>
-      <section className="split">
+const Contact = props => {
+  return (
+    <section id="contact">
+      <div className="inner">
         <section>
-          <div className="contact-method">
-            <span className="icon alt fa-envelope" />
-            <h3>Email</h3>
-            <a href="#">information@untitled.tld</a>
-          </div>
+          <form
+            method="POST"
+            data-netlify="true"
+            action="/success"
+            name="contact"
+          >
+            <input type="hidden" name="form-name" value="contact" />
+            <div className="field half first">
+              <label htmlFor="name">Name</label>
+              <input type="text" name="name" id="name" />
+            </div>
+            <div className="field half">
+              <label htmlFor="email">Email</label>
+              <input type="text" name="email" id="email" />
+            </div>
+            <div className="field">
+              <label htmlFor="message">Message</label>
+              <textarea name="message" id="message" rows="6" />
+            </div>
+
+            <ul className="actions">
+              <li>
+                <input type="submit" value="Send Message" className="special" />
+              </li>
+              <li>
+                <input type="reset" value="Clear" />
+              </li>
+            </ul>
+          </form>
         </section>
-        <section>
-          <div className="contact-method">
-            <span className="icon alt fa-phone" />
-            <h3>Phone</h3>
-            <span>(000) 000-0000 x12387</span>
-          </div>
-        </section>
-        <section>
-          <div className="contact-method">
-            <span className="icon alt fa-home" />
-            <h3>Address</h3>
-            <span>
-              1234 Somewhere Road #5432
-              <br />
-              Nashville, TN 00000
-              <br />
-              United States of America
-            </span>
-          </div>
-        </section>
-      </section>
-    </div>
-  </section>
-)
+        <ContactInfo contactInfoData={props} />
+      </div>
+    </section>
+  )
+}
 
 export default Contact
