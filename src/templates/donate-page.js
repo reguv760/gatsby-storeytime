a1b364610d1e4dee6e16ab63abd593e76db2bf1a
Reginald Galang <reguv760@gmail.com>
2019-04-25 12:36:12 -0700

    add contactEnabled to all pages in templates

--- src/templates/donate-page.js
+++ src/templates/donate-page.js
@@ -10,20 +10,20 @@ export const DonatePageTemplate = ({ title, content, contentComponent }) => {
 
   return (
     <div id="main" className="alt">
-    <section className="section section--gradient">
+      <section className="section section--gradient">
         <div className="inner">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <div className="section">
-              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-                {title}
-              </h2>
-              <PageContent className="content" content={content} />
+          <div className="columns">
+            <div className="column is-10 is-offset-1">
+              <div className="section">
+                <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+                  {title}
+                </h2>
+                <PageContent className="content" content={content} />
+              </div>
             </div>
           </div>
         </div>
-        </div>
-    </section>
+      </section>
     </div>
   )
 }
@@ -39,7 +39,7 @@ const DonatePage = ({ data }) => {
   const { markdownRemark: post } = data
 
   return (
-    <Layout>
+    <Layout contactEnabled={true}>
       <DonatePageTemplate
         contentComponent={HTMLContent}
         title={post.frontmatter.title}
