58431f9626427dba50eff46d7faf3be6a4792cfd
Reginald Galang <reguv760@gmail.com>
2019-04-27 00:00:44 -0700

    fix gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,7 +68,6 @@ module.exports = {
       resolve: `gatsby-plugin-netlify-cms`,
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`,
-        enableIdentityWidget: false,
         htmlTitle: 'StoreyTime CMS',
       },
     },
