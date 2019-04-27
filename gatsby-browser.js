90c56aa6ea087a01de75403812ccfeac0e9591e6
Reginald Galang <reguv760@gmail.com>
2019-04-26 20:02:35 -0700

    fix scrollable anchor bug in gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,4 +4,17 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
-// You can delete this file if you're not using it
+// this is used by react-scrollable-anchor
+// due to gatsby 'bug' that scrolls to top
+// when scrollbar reaches <Scrollable Anchor>
+// without clicks on nav
+exports.shouldUpdateScroll = ({
+  routerProps: { location },
+  getSavedScrollPosition,
+}) => {
+  if (location.hash) {
+    return false
+  }
+
+  return true
+}
