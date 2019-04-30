0c4be0b2722b4bacc7d500275c8c1727f5ca1012
Reginald Galang <reguv760@gmail.com>
2019-04-30 14:20:39 -0700

    add Paypal Component + paypalID to events and CMS

--- src/components/eventlist/eventlist.js
+++ src/components/eventlist/eventlist.js
@@ -8,8 +8,6 @@ export const EventListTemplate = ({ eventListData }) => {
 
   const paypalID = eventListData.paypalID
 
-  console.log(paypalID)
-
   const PageContent = HTMLContent
 
   return (
@@ -19,21 +17,19 @@ export const EventListTemplate = ({ eventListData }) => {
         style={{ listStyle: 'none', margin: '1rem' }}
       >
         <div className="col-3">
+          <h2>Donate Today</h2>
           <p>
-            Ex est eiusmod fugiat tempor incididunt voluptate aliqua veniam
-            cupidatat incididunt minim do aute. Minim anim deserunt adipisicing
-            esse aliquip dolor veniam quis consequat Lorem excepteur cillum
-            deserunt. Quis sunt tempor ex aute consectetur laborum eu excepteur
-            duis exercitation sunt cillum. Sunt consequat qui consectetur
-            adipisicing. Est nostrud qui labore deserunt. Pariatur officia
-            dolore occaecat consectetur. Do ipsum nisi nisi do nostrud amet
-            officia ipsum fugiat laborum. Est cillum commodo duis reprehenderit
-            voluptate occaecat tempor aute irure proident sunt. Culpa ea in
-            adipisicing cupidatat occaecat. Ut sit qui consequat ut cupidatat
-            deserunt tempor cillum aute consectetur exercitation anim. Aliqua ad
-            consectetur adipisicing ullamco ea eiusmod mollit enim exercitation
-            labore.
+            Storeytime shares their story, music and message at autism events,
+            schools and other public programs with no expectation of payment.
+            Storeytime Inc. is currently in need of funding in order to help pay
+            for travel expenses, musical equipment, musicians, website
+            maintenance, recordings, and administrative needs. Thank you so much
+            for your generous support!
           </p>
+          <Paypal
+            paypalID={paypalID}
+            style={{ display: 'block', margin: '0 auto' }}
+          />
         </div>
         <div className="col-9">
           {eventItems.map((events, index) => (
